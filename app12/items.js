56b9b4c0ca57d9407f92c851630356c9e6eea7a4
Noah Seslar Britton <noahseslar@gmail.com>
2023-03-24 10:34:59 -0400

    adding app12

---
+++ app12/items.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        item1: {
+            id: "blue rock",
+            img_url: "app12/imgs/blue_rock.jpg",
+            description: "Blue rock - Micah",
+        },
+        item2: {
+            id: "strand hunter",
+            img_url: "app12/imgs/strand_hunter.jpg",
+            description: "The class I most often play in Destiny 2 right now", 
+        },
+        item3: {
+            id: "fire punch",
+            img_url: "app12/imgs/fire_punch.jpg",
+            description: "Me when playing Lightfall", 
+        },
+        item4: {
+            id: "sigh",
+            img_url: "app12/imgs/sigh.jpg",
+            description: "Me when buying lightfall", 
+        },
+    }
+  ];
+  
+  export default data;
